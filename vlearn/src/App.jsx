6612cda6e476212cd2fd9f5012c565cd58a965f9
GuillaumeLempereur <guillaume.lempereur@hotmail.com>
2026-01-24 15:55:39 +0000

    get words before merge

--- vlearn/src/App.jsx
+++ vlearn/src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
-import FlashCard from './FlashCard.jsx'
+import FlashCard from './FlashCard.jsx';
+import Words from './Words.json';
 
 function App(props){
-  //const rows = Array.from({ length: 20 }, (_, i) => ["Word", `Row ${i + 1}`]);
-  const [visibleRows, setVisibleRows] = useState(Array(20).fill(false));
+	const [words, setWords] = useState([]);
+	const [visibleRows, setVisibleRows] = useState(Array(20).fill(false));
+	//var words = [];
 
   const toggleRow = (index) => {
     setVisibleRows((prev) =>
@@ -21,6 +23,10 @@ function App(props){
 	});
 		const data = await response.json();
 		console.log(data);
+		let w = [];
+		for(let i=0;i<data.Words.length;++i)
+			w.push([Words[data.Words[i][0]][1], Words[data.Words[i][1]][1], data.Words[i][2]]);
+		setWords(w);
 	}
 	sendData();
   return (
@@ -31,7 +37,7 @@ function App(props){
 		<th>English</th>
 		<th>Mongol</th>
 	  </tr>
-        {props.words.map((row, i) => (
+        {words.map((row, i) => (
           <tr key={i}>
             <td>{row[1]}</td>
             <td onClick={() => toggleRow(i)}>
--- vlearn/src/main.jsx
+++ vlearn/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+var words = [];
+
 import Table from './Table.jsx'
-import words from './data.json'
+//import words from './data.json'
 
 /*
  *	Shuffle an array,  to be used with map
@@ -14,11 +17,12 @@ function shuffle(it, i, arr){
 	[arr[i], arr[j]] = [arr[j], arr[i]];
 	return arr[i];
 }
-
+/*
 let rows = words.map((it, i, ar) => shuffle(it, i, ar));
 rows = rows.slice(-20);
 console.log(words[0][1]);
-
+*/
 createRoot(document.getElementById('root')).render(
-	<App words={rows}/>
+	<App/>
 )
+//	<App words={rows}/>
